dccdd3e997ebebeec767730300cd8e2321eaae33
Robin <r.menninga@live.nl>
2020-11-26 10:49:32 +0100

    bug fixes

--- app/src/main/java/com/example/myread/MainActivity.java
+++ app/src/main/java/com/example/myread/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.myread;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -30,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_settings, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_search, R.id.nav_settings)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
