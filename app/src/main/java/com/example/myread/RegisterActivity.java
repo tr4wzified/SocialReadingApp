062d013d0939ee2eaebdff67e42418cf4ec5792e
JanSHoekstra <altlockg@gmail.com>
2020-12-08 11:51:34 +0100

    Change login code to use ServerConnect. Remove helper functions moved to Serverconnect.

--- app/src/main/java/com/example/myread/RegisterActivity.java
+++ app/src/main/java/com/example/myread/RegisterActivity.java
@@ -134,39 +134,6 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
-    private OkHttpClient getUnsafeOkHttpClient(){
-        try {
-            final TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) { }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) { }
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return new java.security.cert.X509Certificate[]{};
-                        }
-                    }
-            };
-
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
-
-            builder.hostnameVerifier((hostname, session) -> true);
-
-            return builder.build();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public void registrationTest() {
         List<String> name = new ArrayList<String>();
         List<String> pass = new ArrayList<String>();
