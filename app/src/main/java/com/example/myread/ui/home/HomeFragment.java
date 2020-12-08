4ded376374476243038747ad91b047dad98f2348
Robin Menninga <r.menninga@live.nl>
2020-12-08 12:11:56 +0100

    Fixed redirect to main

--- app/src/main/java/com/example/myread/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myread/ui/home/HomeFragment.java
@@ -7,9 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myread.R;
@@ -24,7 +22,7 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 }
