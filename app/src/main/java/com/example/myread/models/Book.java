07b9eea006b741b9b081f4e18a7979e9d4727f39
Jacsquare <Jacsquare27@gmail.com>
2020-12-11 17:32:11 +0100

    Creating a book, skips attributes when json is null

--- app/src/main/java/com/example/myread/models/Book.java
+++ app/src/main/java/com/example/myread/models/Book.java
@@ -7,13 +7,13 @@ public class Book {
     private int id = 0;
     public String title;
     public String author;
-    private String cover;
-    private String description;
-    private List<String> subjects;
-    private String publishDate;
-    private String authorWiki;
-    private String isbn;
-    private String rating;
+    public String cover;
+    public String description;
+    public List<String> subjects;
+    public String publishDate;
+    public String authorWiki;
+    public String isbn;
+    public String rating;
 
     public Book(String title, String author, String cover, String description, List<String> subjects, String publishDate, String authorWiki, String isbn, String rating) {
         id++;
