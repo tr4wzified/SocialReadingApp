0c821ccb733d6eaef630c3b76a68c21bb11aa3b3
Jacsquare <Jacsquare27@gmail.com>
2020-12-17 10:33:02 +0100

    d

---
+++ app/src/main/java/com/example/myread/ui/search/SearchActivity.java
@@ -0,0 +1,89 @@
+package com.example.myread.ui.search;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myread.BookActivity;
+import com.example.myread.R;
+import com.example.myread.ServerConnect;
+import com.example.myread.adapters.CollectionAdapter;
+import com.example.myread.models.Book;
+import com.example.myread.models.User;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity implements CollectionAdapter.OnCardListener {
+    protected RecyclerView mRecyclerView;
+    protected CollectionAdapter mAdapter;
+    private List<Book> mCards = new ArrayList<>();
+    protected User user;
+    private String collectionTitle;
+    private String bookTitle;
+
+    //    TextView bookName;
+    Button getBook;
+    TextInputLayout bookUserInput;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_search);
+        mRecyclerView = findViewById(R.id.searchRecyclerView);
+        user = User.getInstance();
+        collectionTitle = getIntent().getStringExtra("collectiontitle");
+
+//        bookName = findViewById(R.id.bookName);
+        getBook = findViewById(R.id.searchBook);
+        bookUserInput = findViewById(R.id.bookSearchQuery);
+
+
+        initRecyclerView();
+//        initBooks();
+
+        getBook.setOnClickListener(v -> showBookResults());
+    }
+
+    private void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        mAdapter = new CollectionAdapter(mCards, this);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void showBookResults() {
+        if (!mCards.isEmpty()) mCards.clear();
+        mCards.addAll(searchBooks());
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private List<Book> searchBooks() {
+        String bookName = bookUserInput.getEditText().getText().toString();
+        return ServerConnect.getInstance().getBooks(bookName);
+    }
+
+    private void initBooks() {
+        mCards.addAll(user.getBookCollection(collectionTitle));
+    }
+
+    @Override
+    public void OnCardClick(int position) {
+//        Book book = mCards.get(position);
+        bookTitle = mCards.get(position).title;
+        Intent intent = new Intent(this, BookActivity.class);
+        intent.putExtra("Book", bookTitle);
+        startActivity(intent);
+    }
+}
