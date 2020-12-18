e5587b3370e1d0f2952ae19c89eead1852aaa96f
Robin Menninga <r.menninga@live.nl>
2020-12-18 09:40:05 +0100

    removed viewmodel classes for being stupid

--- app/src/main/java/com/example/myread/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myread/ui/home/HomeFragment.java
@@ -14,15 +14,7 @@ import com.example.myread.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
