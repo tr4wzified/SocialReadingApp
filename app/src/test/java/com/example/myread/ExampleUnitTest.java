78941204d7139a470d83d4f94d914c3c413bf817
JanSHoekstra <altlockg@gmail.com>
2020-12-18 10:34:12 +0100

    Add test case for password validation.

--- app/src/test/java/com/example/myread/ExampleUnitTest.java
+++ app/src/test/java/com/example/myread/ExampleUnitTest.java
@@ -1,7 +1,6 @@
 package com.example.myread;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
---
+++ app/src/test/java/com/example/myread/RegistrationUnitTest.java
@@ -0,0 +1,34 @@
+package com.example.myread;
+
+import org.junit.Test;
+
+import static com.example.myread.RegisterActivity.passwordComplexityTest;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class RegistrationUnitTest {
+    @Test
+    public void validatePasswordTest()
+    {
+        // Password is minimally 8 chars long.
+        assertFalse(passwordComplexityTest("Pa0!aaa"));
+        // Password needs special char
+        assertFalse(passwordComplexityTest("Passw0rdwithoutspecial"));
+        // Password needs to be 64 chars or less
+        assertFalse(passwordComplexityTest("Passw0rd!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")); //65 chars
+        assertTrue(passwordComplexityTest("Passw0rd!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")); //64 chars
+        // Password needs capital
+        assertFalse(passwordComplexityTest("passw0rd!"));
+        // Password needs number
+        assertFalse(passwordComplexityTest("Password!"));
+
+        // Known correct pass
+        assertTrue(passwordComplexityTest("Passw0rd!"));
+    }
+}
