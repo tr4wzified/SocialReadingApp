69727d773b0e8d756ad4d6f1716619705db23a50
JanSHoekstra <altlockg@gmail.com>
2021-01-05 10:48:57 +0100

    Test ASCII-test function.

---
+++ app/src/test/java/com/example/myread/ASCIIpTest.java
@@ -0,0 +1,31 @@
+package com.example.myread;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ASCIIpTest {
+    @Test
+    public void EmptyIsASCII() {
+        assertTrue(GlobalFunctions.asciip(""));
+    }
+    @Test
+    public void AlphaIsASCII() {
+        assertTrue(GlobalFunctions.asciip("abcdefghijklmnopqrstuvwxyz"));
+    }
+    @Test
+    public void NumberIsASCII() {
+        assertTrue(GlobalFunctions.asciip("1234567890"));
+    }
+    @Test
+    public void SpecialIsASCII() {
+        assertTrue(GlobalFunctions.asciip("!@#$%^&*()_+=-"));
+    }
+    @Test
+    public void DeadIsNotASCII() {
+        assertFalse(GlobalFunctions.asciip("éáô"));
+    }
+    @Test
+    public void EmojiIsNotASCII() {
+        assertFalse(GlobalFunctions.asciip("👌"));
+    }
+}
