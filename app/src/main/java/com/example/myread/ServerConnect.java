d36792919b3803bd059c76ab580dae31e2f8de27
JanSHoekstra <altlockg@gmail.com>
2021-01-12 10:33:04 +0100

    Clean up ServerConnect.

--- app/src/main/java/com/example/myread/ServerConnect.java
+++ app/src/main/java/com/example/myread/ServerConnect.java
@@ -44,6 +44,7 @@ public class ServerConnect extends AppCompatActivity {
     private final OkHttpClient client;
     private final String ip = GlobalApplication.getAppContext().getString(R.string.altaltip);
     private final SharedPreferences prf = GlobalApplication.getEncryptedSharedPreferences();
+    ExecutorService threadPool = newFixedThreadPool(2);
 
     private ServerConnect() {
         client = getUnsafeOkHttpClient();
@@ -117,8 +118,7 @@ public class ServerConnect extends AppCompatActivity {
             if (body != null) request.post(body);
 
             ServerCall c = new ServerCall(client, request.build());
-            ExecutorService e = newFixedThreadPool(1);
-            return (Response) e.submit(c).get();
+            return (Response) threadPool.submit(c).get();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -162,7 +162,7 @@ public class ServerConnect extends AppCompatActivity {
                 for (int b = 0; b < bookArray.length(); b++) {
                     String bookString = bookArray.get(b).toString();
                     Book book = getBookByID(bookString);
-                    if (!(book == null))
+                    if (book != null)
                         bc.initBook(book);
                 }
             }
@@ -171,31 +171,38 @@ public class ServerConnect extends AppCompatActivity {
         }
     }
 
-
     public void initUser(String name) {
         User user = User.getInstance();
         user.name = name;
         Response response = sendGet("user/" + name);
         JSONArray jsonArray = new JSONArray();
         if (response.successful) {
-
-            Thread thread = new Thread(new Runnable() {
-                public void run() {
-                    loadBooks(user, loadBookCollections((user)));
-                }
-            });
-
-            thread.start();
+            threadPool.submit(() -> loadBooks(user, loadBookCollections(user)));
         }
     }
 
+    private Book jsonToBook(JSONObject j) {
+        return new Book(
+                j.optString("id", ""),
+                j.optString("title", ""),
+                j.optString("author", ""),
+                j.optString("cover_img_large", ""),
+                j.optString("cover_img_small", ""),
+                j.optString("cover_img_medium", ""),
+                j.optString("description", ""),
+                getSubjects(j),
+                j.optString("publish_date", ""),
+                j.optString("book_wiki", ""),
+                j.optString("isbn", ""),
+                j.optString("rating", "")
+        );
+    }
+
     public Book getBookByID(String id) {
-        JSONObject jsonObject;
         Response response = sendGet("book/" + id);
         if (response.successful) {
             try {
-                jsonObject = new JSONObject(response.responseString);
-                return new Book(jsonObject.optString("id", ""), jsonObject.optString("title", ""), jsonObject.optString("author", ""), jsonObject.optString("cover_img_large", ""), jsonObject.optString("cover_img_small", ""), jsonObject.optString("cover_img_medium", ""), jsonObject.optString("description", ""), getSubjects(jsonObject), jsonObject.optString("publish_date", ""), jsonObject.optString("book_wiki", ""), jsonObject.optString("isbn", ""), jsonObject.optString("rating", ""));
+                return jsonToBook(new JSONObject(response.responseString));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -205,21 +212,17 @@ public class ServerConnect extends AppCompatActivity {
     }
 
     private List<String> getSubjects(JSONObject jsonObject) {
-        JSONArray subjectsArray;
         List<String> subjects = new ArrayList<>();
         try {
-            if (jsonObject.toString().contains("subjects")) {
-                if (!jsonObject.get("subjects").equals("")) {
-                    subjectsArray = jsonObject.getJSONArray("subjects");
-                    for (int j = 0; j < subjectsArray.length(); j++)
-                        subjects.add(subjectsArray.getString(j));
-                    return subjects;
-                }
+            if (jsonObject.toString().contains("subjects") && !jsonObject.get("subjects").equals("")) {
+                JSONArray subjectsArray = jsonObject.getJSONArray("subjects");
+                for (int j = 0; j < subjectsArray.length(); j++)
+                    subjects.add(subjectsArray.getString(j));
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return new ArrayList<>();
+        return subjects;
     }
 
     public List<Book> getBooks(String bookName) {
@@ -228,11 +231,10 @@ public class ServerConnect extends AppCompatActivity {
         if (response.successful)
             try {
                 JSONArray jsonArray = new JSONArray(response.responseString);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    Book book = new Book(jsonObject.optString("id", ""), jsonObject.optString("title", ""), jsonObject.optString("author", ""), jsonObject.optString("cover_img_large", ""), jsonObject.optString("cover_img_small", ""), jsonObject.optString("cover_img_medium", ""), jsonObject.optString("description", ""), getSubjects(jsonObject), jsonObject.optString("publish_date", ""), jsonObject.optString("book_wiki", ""), jsonObject.optString("isbn", ""), jsonObject.optString("rating", ""));
-                    books.add(book);
-                }
+
+                for (int i = 0; i < jsonArray.length(); i++)
+                    books.add(jsonToBook(jsonArray.getJSONObject(i)));
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -289,8 +291,10 @@ public class ServerConnect extends AppCompatActivity {
             builder.hostnameVerifier((hostname, session) -> true);
             builder.readTimeout(60, TimeUnit.SECONDS);
 
-            ClearableCookieJar cookieJar =
-                    new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(GlobalApplication.getAppContext()));
+            ClearableCookieJar cookieJar = new PersistentCookieJar(
+                    new SetCookieCache(),
+                    new SharedPrefsCookiePersistor(GlobalApplication.getAppContext())
+            );
             builder.cookieJar(cookieJar);
 
             return builder.build();
