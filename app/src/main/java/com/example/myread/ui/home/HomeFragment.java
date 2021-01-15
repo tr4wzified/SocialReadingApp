e2e783a097820ab880306c56f9f0bcf0237fd16f
Jacsquare <Jacsquare27@gmail.com>
2021-01-15 10:18:32 +0100

    Implemented recommended books on homeFRagment

--- app/src/main/java/com/example/myread/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myread/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,32 +16,39 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.myread.R;
 import com.example.myread.ServerConnect;
 import com.example.myread.adapters.BookListItemAdapter;
+import com.example.myread.adapters.RecommendedAdapter;
 import com.example.myread.models.Book;
 import com.example.myread.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragment extends Fragment implements BookListItemAdapter.OnCardListener {
+public class HomeFragment extends Fragment implements BookListItemAdapter.OnCardListener, RecommendedAdapter.OnCardListener {
     private BookListItemAdapter mAdapter;
+    private RecommendedAdapter rAdapter;
     private RecyclerView mRecyclerView;
+    private RecyclerView rRecyclerView;
 
     private Book clickedCard;
     protected User user = User.getInstance();
     private final List<Book> mCards = new ArrayList<>();
-    private final List<Book> recBooks = new ArrayList<>();
-    View recommendedView;
+    private final List<Book> rCards = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        recommendedView = inflater.inflate(R.layout.fragment_recommended, container, false);
         final FragmentActivity c = getActivity();
         mRecyclerView = rootView.findViewById(R.id.bookListItemRecyclerView);
+        rRecyclerView = rootView.findViewById(R.id.recommendRecyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(c);
+        LinearLayoutManager rLayoutManager = new LinearLayoutManager(c, LinearLayoutManager.HORIZONTAL, false);
+
+        rRecyclerView.setLayoutManager(rLayoutManager);
         mRecyclerView.setLayoutManager(layoutManager);
+        rAdapter = new RecommendedAdapter(rCards, HomeFragment.this);
         mAdapter = new BookListItemAdapter(mCards, HomeFragment.this);
         mRecyclerView.setAdapter(mAdapter);
-        mAdapter.notifyDataSetChanged();
+        rRecyclerView.setAdapter(rAdapter);
+
         initCards();
 
         return rootView;
@@ -54,7 +62,8 @@ public class HomeFragment extends Fragment implements BookListItemAdapter.OnCard
     }
 
     public void initRecommended() {
-        recBooks.addAll(getRecommendedBooks());
+        rCards.clear();
+        rCards.addAll(getRecommendedBooks());
 
        //TODO: Show books on screen and make clickable
 
@@ -67,6 +76,14 @@ public class HomeFragment extends Fragment implements BookListItemAdapter.OnCard
     @Override
     public void OnCardClick(int position) {
         clickedCard = mCards.get(position);
+        User.getInstance().setTempBook(clickedCard);
+        Navigation.findNavController(getView()).navigate(R.id.action_nav_home_to_bookFragment);
+    }
+
+
+    @Override
+    public void OnRecCardClick(int position) {
+        clickedCard = rCards.get(position);
         User.getInstance().setTempBook(clickedCard);
         Navigation.findNavController(getView()).navigate(R.id.action_nav_home_to_bookFragment);
     }
